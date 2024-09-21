2624ec57811ccd7b752d92adc1e7793a04256163
hemasundar_oro <hemasundar.k@orotron.com>
2024-09-21 14:40:54 +0530

    add animation for the welcome page

--- frontend/src/components/Welcome/Welcome.js
+++ frontend/src/components/Welcome/Welcome.js
@@ -1,11 +1,11 @@
-import { OrbitControls } from "@react-three/drei";
-import { extend } from "@react-three/fiber";
+import { OrbitControls, Stars } from "@react-three/drei";
+import { Canvas, extend } from "@react-three/fiber";
 import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import Landing from "../../assets/images/Landing.webp";
 import { MeshLine, MeshLineMaterial } from "../Intro/MeshLine";
-
+import Space from "../Intro/Space";
 extend({ MeshLine, MeshLineMaterial, OrbitControls });
 
 const colors = {
@@ -49,18 +49,6 @@ export default function Welcome() {
 
   return (
     <div className="flex flex-col justify-center items-center gap-10 w-full ">
-      {/* <Canvas
-        camera={{ fov: 100, position: [0, 0, 30] }}
-        onCreated={({ gl, size, camera }) => {
-          if (size.width < 600) {
-            camera.position.z = 45;
-          }
-          gl.setClearColor(new THREE.Color("#080f24"));
-        }}
-      >
-        <Space count={1000} colors={colors.sunnyRainbow} />
-        <Stars radius={100} depth={50} count={5000} factor={2} />
-      </Canvas> */}
       <div className="flex flex-col justify-center items-center">
         <h3 className="text-white font-blod font-primary   md:text-[15px] xl:text-[17px] xxl:text-[20px] ">
           Welcome, Dr.{scientistName}.
@@ -70,14 +58,27 @@ export default function Welcome() {
         </h3>
       </div>
       <figure className={figStyle}>
-        <button
-          class="btn  bg-[#1e1e1e] md:text-[14px] xl:text-[16px] xxl:text-[19px] font-primary absolute top-[47%] left-[45%] w-[144px] h-9 bg-white/10 rounded-[23px] border border-white backdrop-blur-sm text-[#fff] hover:bg-white/10 "
-          onClick={() => {
-            navigate("/intro");
-          }}
-        >
-          Enter game
-        </button>
+        <div className="absolute w-full h-full">
+          <Canvas
+            camera={{ fov: 100, position: [0, 0, 30] }}
+            onCreated={({ gl, size, camera }) => {
+              if (size.width < 600) {
+                camera.position.z = 45;
+              }
+            }}
+          >
+            <Space count={1000} colors={colors.sunnyRainbow} />
+            <Stars radius={100} depth={50} count={5000} factor={2} />
+          </Canvas>
+          <button
+            class="btn  bg-[#1e1e1e] md:text-[14px] xl:text-[16px] xxl:text-[19px] font-primary absolute top-[47%] left-[45%] w-[144px] h-9 bg-white/10 rounded-[23px] border border-white backdrop-blur-sm text-[#fff] hover:bg-white/10 "
+            onClick={() => {
+              navigate("/intro");
+            }}
+          >
+            Enter game
+          </button>
+        </div>
         <div className="w-full h-[calc(100vh-11rem)]">
           <img
             src={Landing}
